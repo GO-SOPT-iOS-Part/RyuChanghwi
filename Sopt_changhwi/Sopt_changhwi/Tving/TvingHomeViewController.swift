fed2ed41b044d8c76fa95e0720b24b752a301dca
ryuchanghwi <ckdgnl10@naver.com>
2023-04-15 01:35:44 +0900

    #3 - TvingHomeVC 생성

---
+++ Sopt_changhwi/Sopt_changhwi/Tving/TvingHomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TvingHomeViewController.swift
+//  Sopt_changhwi
+//
+//  Created by 류창휘 on 2023/04/15.
+//
+
+import UIKit
+
+class TvingHomeViewController : UIViewController {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - ACTIONS
+}
+// MARK: - EXTENSIONs
+extension TvingHomeViewController {
+    
+}
+
