72d683d743f53c3fbf97c6b5a78d52a126aa7cf9
ryuchanghwi <ckdgnl10@naver.com>
2023-05-02 21:04:50 +0900

    폴더 구조 수정

---
+++ Sopt_changhwi/Sopt_changhwi/Tving/Sources/Presenter/Main/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  Sopt_changhwi
+//
+//  Created by 류창휘 on 2023/05/02.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - ACTIONS
+}
+// MARK: - EXTENSIONs
+extension MainViewController {
+    
+}
+
